a2fd3b0fb1b498640f54ffa9b9caad861757709d
BradGillis4 <bradgillis4@gmail.com>
2020-12-09 14:02:17 -0500

    commit tree ad93dcd0348703bbd77482909b2767770042a633

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,20 @@
 def oxford_comma(array)
+    if array.length < 2
+    array.join
+    elsif array.length == 2
+    array.join(" and ")
+    else
+        str_test = ""
+        array.each do |i| 
+            if array.index(i) == 0
+                str_test << i
+            elsif array.index(i) > 0 && array.index(i) < (array.length() -1)
+                str_test << ", #{i}"
+            else
+                str_test << ", and #{i}"
+            end
+        end
+return str_test
+end
 
 end
